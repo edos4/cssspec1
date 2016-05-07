c6c07d09f02d3f88beaa62d78b6a0f82841354bd
Ed Salinas <edsalinas7b@yahoo.com>
2016-05-07 12:03:47 +0800

    Add if code

---
+++ if.cbl
@@ -0,0 +1,27 @@
+       identification division.
+       program-id. conditionals.
+       environment division.
+       data division.
+       working-storage section.
+       01 xnuml pic 9(3).
+       01 xnum2 pic 9(3).
+       01 xnum3 pic 9(3).
+       procedure division.
+       evaluate-rtn.
+       display erase.
+       display "enter num-l:" line 5 col 5.
+       display "enter num-2:" line 6 col 5.
+       display "enter num-3:" line 7 col 5.
+      
+       accept xnuml line 5 col 19.
+       accept xnum2 line 6 col 19.
+       accept xnum3 line 7 col 19.
+      
+       if xnuml > xnum2 and xnuml > xnum3
+       	display xnuml line 9 COL 5.
+       IF xnum2 > xnuml AND XNUM2 > XNUM3
+       	display xnum2 line 9 COL 5.
+       IF xnum3 > xnuml AND XNUM3 > XNUM2
+       	display xnum3 line 9 COL 5.
+      
+       STOP RUN.
